36356d5f468f2837b6228d422af3c894ff36da7a
pavankumarch1219 <pavankumarch29@gmail.com>
2026-05-10 22:30:46 +0530

    Create ApplicationTest.java

---
+++ src/test/java/com/pavan/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.pavan;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
